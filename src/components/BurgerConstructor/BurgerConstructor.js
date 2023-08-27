a882bf4c5920250f539596e647091abdd5df5d45
vilivonka <gromootvod@lenta.ru>
2023-08-27 15:30:29 +0300

    two

--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -1,32 +1,72 @@
+import React from 'react';
 import styles from './BurgerConstructor.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import { Typography } from '@ya.praktikum/react-developer-burger-ui-components';
 import { Box } from '@ya.praktikum/react-developer-burger-ui-components';
-import bun1 from '../../images/bun-01.png';
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 
-function BurgerConstructor() {
-
+function BurgerConstructor({ data }) {
+  const [current, setCurrent] = React.useState('Булки');
+  const arrBun = data.filter(el => el.type === 'bun');
+  const arrSause = data.filter(el => el.type === 'sauce');
+  const arrMain = data.filter(el => el.type === 'main');
   return (
     <div className={styles.box}>
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
       <div className={styles.tab}>
         <div style={{ display: 'flex' }}>
-          <Tab >Булки</Tab>
-          <Tab >Соусы</Tab>
-          <Tab >Начинки</Tab>
+          <Tab value="Булки" active={current === 'Булки'} onClick={setCurrent}>
+            Булки
+          </Tab>
+          <Tab value="Соусы" active={current === 'Соусы'} onClick={setCurrent}>
+            Соусы
+          </Tab>
+          <Tab value="Начинки" active={current === 'Начинки'} onClick={setCurrent}>
+            Начинки
+          </Tab>
         </div>
       </div>
-      <div className= {styles.tab_box}>
+      <div className={styles.tab_box}>
         <h3 className="mb-6 mt-10 text text_type_main-medium">Булки</h3>
-        <div className={styles.tab_box_wrap}>
-          <div className= {styles.tab_box_wrap_item}>
-            <img alt='img' src= {bun1}/>
-            <div className= {styles.price}>
-              <span>20</span>
-            </div>
-          </div>
-        </div>
+        <ul className={styles.tab_box_wrap}>
+          {arrBun.map(el =>
+            <li key={el._id} className={`${styles.tab_box_item} mb-8`}>
+                <img src={el.image} alt={'img'} />
+                <div className={`${styles.price} mt-1 mb-4`}>
+                  <p className={'text text_type_digits-default mr-2'}>{el.price}</p>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span className={`${styles.name} mt-1 text text_type_main-default`}>{el.name}</span>
+            </li>
+          )}
+        </ul>
+        <h3 className="mb-6 mt-10 text text_type_main-medium">Соусы</h3>
+        <ul className={styles.tab_box_wrap}>
+          {arrSause.map(el =>
+            <li key={el._id} className={`${styles.tab_box_item} mb-8`}>
+                <img src={el.image} alt={'img'} />
+                <div className={`${styles.price} mt-1 mb-4`}>
+                  <p className={'text text_type_digits-default mr-2'}>{el.price}</p>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span className={`${styles.name} mt-1 text text_type_main-default`}>{el.name}</span>
+            </li>
+          )}
+        </ul>
+        <h3 className="mb-6 mt-10 text text_type_main-medium">Начинки</h3>
+        <ul className={styles.tab_box_wrap}>
+          {arrMain.map(el =>
+            <li key={el._id} className={`${styles.tab_box_item} mb-8`}>
+                <img src={el.image} alt={'img'} />
+                <div className={`${styles.price} mt-1 mb-4`}>
+                  <p className={'text text_type_digits-default mr-2'}>{el.price}</p>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span className={`${styles.name} mt-1 text text_type_main-default`}>{el.name}</span>
+            </li>
+          )}
+        </ul>
       </div>
     </div>
   )
